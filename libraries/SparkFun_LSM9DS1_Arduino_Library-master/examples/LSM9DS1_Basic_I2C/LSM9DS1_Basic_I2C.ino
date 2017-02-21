96350a03d7d3b0f778308678bf919de2e5c2aa01
edgarw19 <edgar@dispatch.me>
2017-02-21 16:49:28 -0500

    update

--- libraries/SparkFun_LSM9DS1_Arduino_Library-master/examples/LSM9DS1_Basic_I2C/LSM9DS1_Basic_I2C.ino
+++ libraries/SparkFun_LSM9DS1_Arduino_Library-master/examples/LSM9DS1_Basic_I2C/LSM9DS1_Basic_I2C.ino
@@ -1,60 +1,11 @@
-/*****************************************************************
-LSM9DS1_Basic_I2C.ino
-SFE_LSM9DS1 Library Simple Example Code - I2C Interface
-Jim Lindblom @ SparkFun Electronics
-Original Creation Date: April 30, 2015
-https://github.com/sparkfun/LSM9DS1_Breakout
-
-The LSM9DS1 is a versatile 9DOF sensor. It has a built-in
-accelerometer, gyroscope, and magnetometer. Very cool! Plus it
-functions over either SPI or I2C.
-
-This Arduino sketch is a demo of the simple side of the
-SFE_LSM9DS1 library. It'll demo the following:
-* How to create a LSM9DS1 object, using a constructor (global
-  variables section).
-* How to use the begin() function of the LSM9DS1 class.
-* How to read the gyroscope, accelerometer, and magnetometer
-  using the readGryo(), readAccel(), readMag() functions and 
-  the gx, gy, gz, ax, ay, az, mx, my, and mz variables.
-* How to calculate actual acceleration, rotation speed, 
-  magnetic field strength using the calcAccel(), calcGyro() 
-  and calcMag() functions.
-* How to use the data from the LSM9DS1 to calculate 
-  orientation and heading.
-
-Hardware setup: This library supports communicating with the
-LSM9DS1 over either I2C or SPI. This example demonstrates how
-to use I2C. The pin-out is as follows:
-	LSM9DS1 --------- Arduino
-	 SCL ---------- SCL (A5 on older 'Duinos')
-	 SDA ---------- SDA (A4 on older 'Duinos')
-	 VDD ------------- 3.3V
-	 GND ------------- GND
-(CSG, CSXM, SDOG, and SDOXM should all be pulled high. 
-Jumpers on the breakout board will do this for you.)
-
-The LSM9DS1 has a maximum voltage of 3.6V. Make sure you power it
-off the 3.3V rail! I2C pins are open-drain, so you'll be 
-(mostly) safe connecting the LSM9DS1's SCL and SDA pins 
-directly to the Arduino.
-
-Development environment specifics:
-	IDE: Arduino 1.6.3
-	Hardware Platform: SparkFun Redboard
-	LSM9DS1 Breakout Version: 1.0
-
-This code is beerware. If you see me (or any other SparkFun 
-employee) at the local, and you've found our code helpful, 
-please buy us a round!
-
-Distributed as-is; no warranty is given.
-*****************************************************************/
+ 
 // The SFE_LSM9DS1 library requires both Wire and SPI be
 // included BEFORE including the 9DS1 library.
 #include <Wire.h>
 #include <SPI.h>
 #include <SparkFunLSM9DS1.h>
+#include "HX711.h"
+#include "TimerOne.h"
 
 //////////////////////////
 // LSM9DS1 Library Init //
@@ -67,63 +18,209 @@ LSM9DS1 imu;
 // Example I2C Setup //
 ///////////////////////
 // SDO_XM and SDO_G are both pulled high, so our addresses are:
-#define LSM9DS1_M	0x1E // Would be 0x1C if SDO_M is LOW
-#define LSM9DS1_AG	0x6B // Would be 0x6A if SDO_AG is LOW
+#define LSM9DS1_M  0x1E // Would be 0x1C if SDO_M is LOW
+#define LSM9DS1_AG  0x6B // Would be 0x6A if SDO_AG is LOW
+
+/* SET UP THE WEIGHT SENSORS */
+#define DOUT  3
+#define CLK  2
+#define DOUT2 4
+#define CLK2 5
+
+HX711 forwardScale(DOUT, CLK);
+HX711 backwardScale(DOUT2, CLK2);
+
+float calibration_factor = 9300; //-7050 worked for my 440lb max scale setup
+int RIDERWEIGHT = 15;
+
+
+/* Set up the throttle connections */
+char Dword;
+int dutyRate = 82;
+int BASEDUTY = 82;
+int MAXDUTY = 104;
+int MINDUTY = 60;
+int DUTYRANGE = 23;
+int PERIOD = 18800;
+int SPEEDPIN = 9;
+int UNKNOWNPIN = 10;
+int increaseRate = 2;
+int counter = 0;
+//change this number with numberOfReadings
+int dutyReadings[15];
+int numberOfReadings = 15;
+boolean riderIsOn = false;
+unsigned long startTime = 0;
+int constStart = 3;
+
 
 ////////////////////////////
 // Sketch Output Settings //
 ////////////////////////////
 #define PRINT_CALCULATED
 //#define PRINT_RAW
-#define PRINT_SPEED 250 // 250 ms between prints
+#define PRINT_SPEED 1200 // 250 ms between prints
 
 // Earth's magnetic field varies by location. Add or subtract 
 // a declination to get a more accurate heading. Calculate 
 // your's here:
 // http://www.ngdc.noaa.gov/geomag-web/#declination
-#define DECLINATION -8.58 // Declination (degrees) in Boulder, CO.
+#define DECLINATION -12 // Declination (degrees) in Boulder, CO.
 
 void setup() 
 {
   
-  Serial.begin(115200);
+  Serial.begin(9600);
   
-  // Before initializing the IMU, there are a few settings
-  // we may need to adjust. Use the settings struct to set
-  // the device's communication mode and addresses:
+  // Initialize IMU
   imu.settings.device.commInterface = IMU_MODE_I2C;
   imu.settings.device.mAddress = LSM9DS1_M;
   imu.settings.device.agAddress = LSM9DS1_AG;
-  // The above lines will only take effect AFTER calling
-  // imu.begin(), which verifies communication with the IMU
-  // and turns it on.
-  if (!imu.begin())
-  {
-    Serial.println("Failed to communicate with LSM9DS1.");
-    Serial.println("Double-check wiring.");
-    Serial.println("Default settings in this sketch will " \
-                  "work for an out of the box LSM9DS1 " \
-                  "Breakout, but may need to be modified " \
-                  "if the board jumpers are.");
-    while (1)
-      ;
+
+  // Initialize scale
+  forwardScale.set_scale(calibration_factor);
+  forwardScale.tare();
+  backwardScale.set_scale(calibration_factor); 
+  backwardScale.tare(); 
+  Serial.println("WTF");
+  Serial.println("Readings:");
+//  if (!imu.begin())
+//  {
+//    Serial.println("Failed to communicate with LSM9DS1.");
+//    Serial.println("Double-check wiring.");
+//    Serial.println("Default settings in this sketch will " \
+//                  "work for an out of the box LSM9DS1 " \
+//                  "Breakout, but may need to be modified " \
+//                  "if the board jumpers are.");
+//    while (1)
+//      ;
+//  }
+  Timer1.initialize(500000); 
+  Timer1.setPeriod(PERIOD); //initialize timer1, and set a 1/2 second period
+  Timer1.pwm(SPEEDPIN, dutyRate); // setup pwm on pin 9, 50% duty cycle
+  Timer1.pwm(UNKNOWNPIN, 81);
+  for (int i = 0; i < numberOfReadings; i++){
+    dutyReadings[i] = 82;
   }
+  Serial.println("FINISHED SET UP");
+}
+
+int findAverage(int dutyReadings[]){
+  double average = 0;
+  for (int i = 0; i < numberOfReadings; i++){
+//    Serial.print(dutyReadings[i]);
+    average += dutyReadings[i];
+  }
+  return int(average/numberOfReadings);
 }
 
 void loop()
 {
-  printGyro();  // Print "G: gx, gy, gz"
-  printAccel(); // Print "A: ax, ay, az"
-  printMag();   // Print "M: mx, my, mz"
-  
-  // Print the heading and orientation for fun!
-  // Call print attitude. The LSM9DS1's magnetometer x and y
-  // axes are opposite to the accelerometer, so my and mx are
-  // substituted for each other.
-  printAttitude(imu.ax, imu.ay, imu.az, -imu.my, -imu.mx, imu.mz);
-  Serial.println();
-  
-  delay(PRINT_SPEED);
+  counter += 1;
+ 
+  //Read the current weight
+  double backWeight = backwardScale.get_units();
+  double forwardWeight = forwardScale.get_units();
+  RIDERWEIGHT = forwardWeight + backWeight;
+
+  //MASSIVE PRINT STATEMENT
+  if (counter % 4 == 0){
+//  Serial.print(backWeight, 2);
+//  Serial.print(",");
+//  Serial.print(forwardWeight, 2); //scale.get_units() returns a float
+//  Serial.print(",");
+//  Serial.print(dutyRate);
+//  Serial.println();
+//  // Print the heading and orientation for fun!
+//  // Call print attitude. The LSM9DS1's magnetometer x and y
+//  // axes are opposite to the accelerometer, so my and mx are
+//  // substituted for each other.
+//  printAttitude(imu.ax, imu.ay, imu.az, -imu.my, -imu.mx, imu.mz);
+//  Serial.println();
+//  Serial.println();
+//  Serial.println(forwardWeight);
+//  Serial.println(forwardWeight*1.0/(backWeight + forwardWeight));
+//  printAccel(); // Print "A: ax, ay, az"
+  }
+
+  //If rider isn't on, don't run the motor
+  if (RIDERWEIGHT < 70){
+    dutyRate = BASEDUTY;
+    riderIsOn = false;
+    Serial.println("CLOSED");
+  }
+  else if (RIDERWEIGHT > 100 && !riderIsOn){
+    riderIsOn = true;
+    startTime = millis();
+    Serial.println("STARTED ENGINE");
+  }
+
+  //If rider is on, calculate the right speed
+  else {
+    double maxWeight = forwardWeight;
+    boolean maxIsForward = true;
+
+    //Calculate which direction rider is leaning
+    if (backWeight > forwardWeight){
+      maxWeight = backWeight;
+      maxIsForward = false;
+    }
+
+    //Check if rider is in neutral position
+    if (maxWeight < .55*RIDERWEIGHT){
+      int blah = 0;
+      dutyRate = BASEDUTY;
+//      Serial.println("RIDER IS NEUTRAL");
+    }
+
+    //If not, drive the motor
+    else {
+      //Calculate the current pulse width
+      double speedFraction = 0;
+      if (maxIsForward){
+        speedFraction = (maxWeight - .55*RIDERWEIGHT)/(.32*RIDERWEIGHT);
+      }
+      else {
+        speedFraction = (maxWeight - .55*RIDERWEIGHT)/(.25*RIDERWEIGHT);
+      }
+//      Serial.println(speedFraction);
+      if (speedFraction > 1) speedFraction = 1.0;
+//      Serial.print("SPEEDS: ");
+//      Serial.print(speedFraction);
+//      Serial.print(", ");
+      speedFraction = pow(speedFraction, 2.5);
+//      Serial.println(speedFraction);
+      int extraPulseWidth = speedFraction * (DUTYRANGE-constStart) + constStart;
+//      Serial.print("Pulse Width");
+//      Serial.println(extraPulseWidth);
+      
+
+      //Determine which direction to drive the motor
+      if (maxIsForward){
+        dutyRate = BASEDUTY + extraPulseWidth;
+      }
+      else {
+        dutyRate = BASEDUTY - extraPulseWidth;
+      }
+
+      //Set thresholding to prevent going too fast
+      if (dutyRate > MAXDUTY){
+        dutyRate = MAXDUTY-5;
+      }
+      if (dutyRate < MINDUTY){
+        dutyRate = MINDUTY+5;
+      }
+    }
+  }
+
+  if (riderIsOn && (millis() - startTime) > 3000){
+    int pos = counter % numberOfReadings;
+    dutyReadings[pos] = dutyRate;
+
+    //Drive the PWM
+    int averageDutyRate = findAverage(dutyReadings);
+    Timer1.pwm(SPEEDPIN, averageDutyRate);
+  }
 }
 
 void printGyro()
@@ -142,10 +239,7 @@ void printGyro()
   // DPS. Give the function the value that you want to convert.
   Serial.print(imu.calcGyro(imu.gx), 2);
   Serial.print(", ");
-  Serial.print(imu.calcGyro(imu.gy), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcGyro(imu.gz), 2);
-  Serial.println(" deg/s");
+  Serial.println(imu.calcGyro(imu.gy), 2);
 #elif defined PRINT_RAW
   Serial.print(imu.gx);
   Serial.print(", ");
@@ -171,10 +265,7 @@ void printAccel()
   // g's. Give the function the value that you want to convert.
   Serial.print(imu.calcAccel(imu.ax), 2);
   Serial.print(", ");
-  Serial.print(imu.calcAccel(imu.ay), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcAccel(imu.az), 2);
-  Serial.println(" g");
+  Serial.println(imu.calcAccel(imu.ay), 2);
 #elif defined PRINT_RAW 
   Serial.print(imu.ax);
   Serial.print(", ");
@@ -246,4 +337,4 @@ void printAttitude(float ax, float ay, float az, float mx, float my, float mz)
   Serial.print(", ");
   Serial.println(roll, 2);
   Serial.print("Heading: "); Serial.println(heading, 2);
-}
+} 
